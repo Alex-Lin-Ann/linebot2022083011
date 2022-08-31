ccb240b9d80be6f2f8db452a8f7a4c0825c904bb
Alex780106 <show780106@gmail.com>
2022-08-31 15:21:21 +0800

    linebot2022083011

--- app.py
+++ app.py
@@ -206,6 +206,9 @@ def getCallCarMessage(data):
 
 def getPlayStickerMessage():
     message = dict()
+    message["type"] = "sticker"
+    message["packageId"] = "446"
+    message["stickerId"] = "1988"
     return message
 
 
